64a46803753109604d116f24a34ca0ccdd47fa99
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-05-13 11:39:21 -0700

    fix CI issue

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -1,7 +1,9 @@
 export const fetchArticles = async (query) => {
     const res = await fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${process.env.API_KEY}`);
     const { articles } = await res.json();
- 
+
+ console.log('!!!!!!!!!!!!!!!', articles);
+
     return articles.map((article) => ({
         title: article.title,
         urlToImage: article.urlToImage,
@@ -9,4 +11,4 @@ export const fetchArticles = async (query) => {
         description: article.description,
         url: article.url,
     }));
-};
+}
